5919b3c574eb5415e5b53311e00795e623ac2594
Sandesh Adhikari <sandeshadhikari92@gmail.com>
2023-10-03 18:06:01 +1000

    Arraylist to student marks, names.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -12,7 +12,31 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+class Student {
+    String lastName;
+    String firstName;
+    String studentID;
+    double a1;
+    double a2;
+    double a3;
+
+    public Student(String lastName, String firstName, String studentID, double a1, double a2, double a3) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.studentID = studentID;
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
+
+
+}
+
 public class StudentStatistics
 {
-    
+    private List<Student> students;
+
+    public StudentStatistics() {
+        students = new ArrayList<>();
+}
 }
